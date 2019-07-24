936871694ffa14faa1af01181261937d8d7b189c
Matt Pitner <mattpitner09@gmail.com>
2019-07-24 20:58:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  [1,2,3,4]
+  array.push 5
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  [1,2,3,4,]
+  array.unshift 0 
 end
 
 def remove_element_from_end_of_array(array)
